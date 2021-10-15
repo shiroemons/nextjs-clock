4448e6031e77ae51ec30e6ddf113dc46282493e6
shiroemons <shiroemons@gmail.com>
2021-10-15 21:55:06 +0900

    PWA support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa")
+const runtimeCaching = require("next-pwa/cache")
+
+const config = {
   reactStrictMode: true,
   experimental: {
     optimizeFonts: true,
   },
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
 }
+
+module.exports = withPWA(config)
